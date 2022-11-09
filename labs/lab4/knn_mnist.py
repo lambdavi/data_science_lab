fdeab405225fe7e11fb7afef05ee7981dafd2f5d
Davide Buoso <daviibuoso@gmail.com>
2022-11-09 15:31:42 +0100

    Modified KNN, improved performance

--- labs/lab4/knn_mnist.py
+++ labs/lab4/knn_mnist.py
@@ -2,7 +2,6 @@ from collections import Counter
 
 import numpy as np
 import pandas as pd
-from sklearn.metrics import confusion_matrix
 
 
 # --- CLASSES AND FUNCTIONS SECTION --
@@ -20,8 +19,9 @@ def find_differences(test, prediction):
     print(f"Accuracy: {result[0]/len(test)}")
 
 class KNearestNeighbors:
-    def __init__(self, k, distance_metric="euclidean"):
+    def __init__(self, k, distance_metric="euclidean",  weights="uniform"):
         self.k = k
+        self.weights = weights
         self.distance_metric = distance_metric
 
     def fit(self, X, y):
@@ -49,43 +49,31 @@ class KNearestNeighbors:
                 predicted_labels.append(self.compute_manhattan(x))
         return predicted_labels
 
-    def take_k(self, support_list):
-        support_list.sort(key=lambda x: x[1])
-        support_list=support_list[:self.k]
-        new_labels = [self.Y_t[s[0]] for s in support_list]
-        c = Counter(new_labels)
+    def compute_euclidean(self, v):
+        votes = self.Y_t[((((self.X_t - v)**2).sum(axis=1))**.5).argsort()[:self.k]] # euclidean distance
+        c = Counter(votes)
         return c.most_common(1)[0][0]
 
-    def compute_euclidean(self, v):
-        support_list = []
-        for i, w in enumerate(self.X_t):            
-            sum_sq = np.sum(np.square(v - w))
-            support_list.append([i, np.sqrt(sum_sq)])
-        return self.take_k(support_list)
-
-    def compute_cosine(self, v):
-        support_list = []
-        for i, w in enumerate(self.X_t):            
-            num = np.sum(np.multiply(v,w))
-            denom_v = np.sum(v**2)**(1/2)
-            denom_w = np.sum(w**2)**(1/2)
-            denom = denom_v*denom_w
-            support_list.append([i, 1 - num/denom])
-        return self.take_k(support_list)
+    def compute_cosine(self, v):     
+        num = (self.X_t*v).sum(axis=1)
+        dv = ((v**2).sum())**.5
+        dw = ((self.X_t**2).sum(axis=1))**.5
+        denom = dv*dw
+        support_list = (1 - np.absolute(num/denom))
+        votes = self.Y_t[support_list.argsort()[:self.k]]
+        c = Counter(votes)
+        return c.most_common(1)[0][0]
 
     def compute_manhattan(self, v):
-        support_list = []
-        for i, w in enumerate(self.X_t):            
-            sum = np.sum(np.absolute(v - w))
-            support_list.append([i, sum])
-        return self.take_k(support_list)
+        votes = self.Y_t[np.absolute(self.X_t - v).sum(axis=1).argsort()[:self.k]]
+        c = Counter(votes)
+        return c.most_common(1)[0][0]
 
     
 # -- MAIN SECTION -- 
 if __name__=="__main__":
     # Reading the dataset
-
-    # MNIST dataset
+    # Iris dataset
     df = pd.read_csv(
         "https://raw.githubusercontent.com/dbdmg/data-science-lab/master/datasets/mnist_test.csv",
         header=None,
@@ -108,14 +96,14 @@ if __name__=="__main__":
     print(f"X test {X_test.shape}")
     y_test = test[0]
     print(f"y test {y_test.shape}")
-
     k = 4
     K_n_e = KNearestNeighbors(k, "euclidean")
     K_n_c = KNearestNeighbors(k, "cosine")
     K_n_m = KNearestNeighbors(k, "manhattan")
 
-    K_n_e.fit(X_train, y_train)   
+       
     K_n_c.fit(X_train, y_train)
+    K_n_e.fit(X_train, y_train)
     K_n_m.fit(X_train, y_train)
 
     print("COSINE")
